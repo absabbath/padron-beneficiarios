a5f38a5aae1e7c968da3be8504ecdf1d4583a69d
Arkam <arkam@localhost.localdomain>
2015-07-16 14:37:33 -0500

    creacion de tabla programas

--- app/database/migrations/2015_07_16_131413_create_programas.php
+++ app/database/migrations/2015_07_16_131413_create_programas.php
@@ -12,7 +12,14 @@ class CreateProgramas extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('programas', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('nombre_programa',128);
+			$table->integer('id_dependencia')->unsigned();
+			$table->timestamps();
+			$table->foreign('id_dependencia')->references('id')->on('dependencias')->onDelete('cascade');
+		});
 	}
 
 	/**
@@ -22,7 +29,7 @@ class CreateProgramas extends Migration {
 	 */
 	public function down()
 	{
-		//
+		Schema::drop('programas');
 	}
 
 }
